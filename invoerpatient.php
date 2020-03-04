14e8c7186ecfbc362fce7d8c7bd06092c703192f
wooyh2000 <61496463+wooyh2000@users.noreply.github.com>
2020-03-04 20:09:30 +0100

    Update invoerpatient.php

--- invoerpatient.php
+++ invoerpatient.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php include '/website-voor-nao/style.css';?>
+<link rel="stylesheet" href='/website-voor-nao/style.css'/>
 </head>
 <title>invoer patient</title>
 <body>
