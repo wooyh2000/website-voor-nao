70c3695e9f7235507c35270a7b2a5b8b627e8930
wooyh2000 <61496463+wooyh2000@users.noreply.github.com>
2020-03-05 15:31:13 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'oege.ie.hva.nl');
-   define('DB_USERNAME', 'wooyh');
-   define('DB_PASSWORD', 'vyrdBMHRSmNtJx');
-   define('DB_DATABASE', 'zwooyh');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
