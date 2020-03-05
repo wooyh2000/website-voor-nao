238ea82b47b421f37052bee5c5b980cede6e8363
wooyh2000 <61496463+wooyh2000@users.noreply.github.com>
2020-03-05 15:33:50 +0100

    Update home.php

--- home.php
+++ home.php
@@ -1,6 +1,4 @@
-<?php
-   include('session.php');
-?>
+
 <html">
    
    <head>
@@ -8,8 +6,7 @@
    </head>
    
    <body>
-      <h1>Welcome <?php echo $login_session; ?></h1> 
-      <h2><a href = "logout.php">Sign Out</a></h2>
+      <p> welkom in bejaardetehuis</p>
    </body>
    
 </html>
