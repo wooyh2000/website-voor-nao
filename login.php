0cd8a87ba4c2c9565b8dcb5b9f954bde77309533
wooyh2000 <61496463+wooyh2000@users.noreply.github.com>
2020-03-05 15:23:06 +0100

    Update login.php

--- login.php
+++ login.php
@@ -57,7 +57,7 @@
 				
             <div style = "margin:30px">
                
-               <form action = "" method = "post">
+               <form action = "https://wooyh2000.github.io/website-voor-nao/inlogmedewerker" method = "post">
                   <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
                   <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
